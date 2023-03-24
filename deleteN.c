91d7efe576def6ddcc4dc432a5e8e04610149c3f
Timishot <timidotcom58@gmail.com>
2023-03-24 17:57:14 +0100

    learnt how to delete a node from a singly list

---
+++ deleteN.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node *link;
+};
+
+struct node *del_first(struct node *head);
+int main(){
+	struct node *head = malloc(sizeof(struct node));
+	head->data=45;
+	head->link=NULL;
+
+	struct node *ptr = malloc(sizeof(struct node));
+	ptr->data=98;
+	ptr-> link=NULL;
+
+	struct node *ptr2 = malloc(sizeof(struct node));
+	ptr2->data=3;
+	ptr2->link=NULL;
+
+	ptr->link=ptr2;
+
+	head->link=ptr;
+
+	head = del_first(head);
+	ptr = head;
+	while(ptr != NULL)
+	{
+		printf("%d ", ptr->data);
+		ptr = ptr->link;
+	}
+	return 0;
+}
+
+struct node *del_first(struct node *head)
+{
+	if(head==NULL)
+		printf("List is already empty!");
+	else
+	{
+		struct node *temp = head;
+		head = head->link;
+		free(temp);
+		temp = NULL;
+	}
+	return head;
+};
