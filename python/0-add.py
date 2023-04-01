36a0a03e408d3531710a0debc2b9a5526de205ff
timishot <timishot@ubuntu.myguest.virtualbox.org>
2023-04-01 16:43:20 +0100

    learing more on modules implementation

--- python/0-add.py
+++ python/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 from add_0 import add
+a, b = input("Enter 2 value to be add : ").split()
+a = int(a)
+b = int(b)
+print("{} + {} = {}".format(a, b, add(a,b)))
 
-ADD=add(1, 4)
 
 
 
-print(ADD)
