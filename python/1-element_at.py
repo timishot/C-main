e8296b1599b994d67654cd3811f11c205fa6da80
timishot <timidotcom58@gmail.com>
2023-04-04 13:00:01 +0100

    made changes to my function learned more on __import__ function

--- python/1-element_at.py
+++ python/1-element_at.py
@@ -2,12 +2,10 @@
 
 def element_at(idx, my_list):
 
-    listnum = len(my_list)
-    if idx < 0 or idx >=listnum:
-        return None
     
+    if idx < 0 or idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
 
-    print(my_list[idx])
 
-my_list = [1, 3, 4, 6, 7,8, 8]
-element_at(3, my_list)
