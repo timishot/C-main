6ab4a8cb51f7c5ecf20ad230cba440c812622f89
timishot <timidotcom58@gmail.com>
2023-04-18 09:44:47 +0100

    More on switch statement

---
+++ java/NestedSwitchExample.java
@@ -0,0 +1,6 @@
+public class NestedSwitchExamplea{
+	public enum Day{Sun, Mon, Tue, Wed, Thu, Fri, Sat}
+	public static void main(String args[]){
+		Day[]DayNow =Day.values();
+		for(Day NOw: DayNow)
+
---
+++ java/SwitchStringExample.java
@@ -0,0 +1,21 @@
+public class SwitchStringExample{
+	public static void main(String args[]){
+		String levelString = "Expert";
+		int level = 0;
+		switch(levelString){
+			case "Beginner":
+				level=1;
+				break;
+			case "Intermediate":
+				level=2;
+				 break;
+			case "Expert":
+				level=3;
+				break;
+			default: 
+				level=0;
+				break;
+		}
+		System.out.println("YOur level is: " + level);
+	}
+}
