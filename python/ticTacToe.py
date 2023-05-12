a897faa2cacfeb13951a18e75c287a983d416a2a
timishot <timidotcom58@gmail.com>
2023-05-12 16:34:09 +0100

    created a function

--- python/ticTacToe.py
+++ python/ticTacToe.py
@@ -27,6 +27,27 @@ for i in range (9):
         turn = 'O'
     else:
         turn = 'X'
+
+    if (theBoard['1'] == theBoard['2'] == theBoard['3'] == 'X' or
+        theBoard['4'] == theBoard['5'] == theBoard['6'] == 'X' or
+        theBoard['7'] == theBoard['8'] == theBoard['9'] == 'X' or
+        theBoard['1'] == theBoard['4'] == theBoard['7'] == 'X' or
+        theBoard['2'] == theBoard['5'] == theBoard['8'] == 'X' or
+        theBoard['3'] == theBoard['6'] == theBoard['9'] == 'X' or
+        theBoard['1'] == theBoard['5'] == theBoard['9'] == 'X' or
+        theBoard['3'] == theBoard['5'] == theBoard['7'] == 'X'):
+        print("Congratulations! Player X wins!")
+        break
+
+    if (theBoard['1'] == theBoard['2'] == theBoard['3'] == 'O' or
+        theBoard['4'] == theBoard['5'] == theBoard['6'] == 'O' or
+        theBoard['7'] == theBoard['8'] == theBoard['9'] == 'O' or
+        theBoard['1'] == theBoard['4'] == theBoard['7'] == 'O' or
+        theBoard['2'] == theBoard['5'] == theBoard['8'] == 'O' or
+        theBoard['3'] == theBoard['5'] == theBoard['7'] == 'O'):
+        print("Congratulations! Player O wins!")
+        break
+
 printBoard(theBoard)
 
 
