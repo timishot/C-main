16ab1d8a586d8a6ffcf71b933bca6b8f1d0a9a41
timishot <timidotcom58@gmail.com>
2023-05-16 21:17:30 +0100

    updateed the function '

---
+++ python/asterisk.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import pyinputplus as pyip
+
+help(pyip.parameters)
--- python/input.py
+++ python/input.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import pyinputplus as pyip
 
-response = pyip.inputNum()
+response = pyip.inputNum(limit=2,timeout=10, default='N/A')
 
