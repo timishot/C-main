5175b23af35225d678936e253f2dc28a1480dbea
timishot <timidotcom58@gmail.com>
2023-05-22 16:02:20 +0100

    still try to debug the code

---
+++ 4-free_list.c
@@ -0,0 +1,11 @@
+#include "list.h"
+void free_list(list_t *head){
+	list_t *current;
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
