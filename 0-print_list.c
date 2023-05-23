c269e3d675465a8ce96f467f19dd7a11397c6017
timishot <timidotcom58@gmail.com>
2023-05-23 21:14:43 +0100

    testing 0-print_list

--- 0-print_list.c
+++ 0-print_list.c
@@ -23,3 +23,16 @@ size_t print_list(const list_t *h)
 	return(count);
 }
 
+int main(){
+	list_t *head = malloc(sizeof(list_t));
+	char str[] = "time warp";
+	head->str = str;
+	head->len =strlen(str);
+	head->next=NULL;
+
+	print_list(head);
+
+
+	return 0;
+}
+
