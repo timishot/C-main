5994deee173211dbb229d8bd0eaedc77b1acbfa9
timishot <timidotcom58@gmail.com>
2023-05-23 21:38:21 +0100

    finally fix the bug

--- 3-add_node_end.c
+++ 3-add_node_end.c
@@ -3,16 +3,29 @@
 list_t *add_node_end(list_t **head, const char *str){
 	list_t *new_node = malloc(sizeof(list_t));
 	list_t *ptr;
-	ptr= *head;
-	if (*head == NULL){
+
+	if (new_node == NULL)
+		return NULL;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL){
+		free(new_node);
 		return NULL;
 	}
-	while(ptr->next!=NULL){
-		ptr=ptr->next;
-	}
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len =strlen(str);
 	new_node->next = NULL;
-	ptr->next =new_node;	
+
+	if (*head == NULL){
+		*head = new_node;
+		return(new_node);
+	}
+	else{
+		ptr = *head;
+		while(ptr->next != NULL)
+		{
+			ptr=ptr->next;
+		}
+		ptr->next = new_node;
+	}
+
 	return new_node;
 }
