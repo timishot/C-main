90396cbda6e258ba37c868179a6d645bc41ddf53
timishot <timidotcom58@gmail.com>
2023-06-04 21:39:01 +0100

    Worked on a bytecode

---
+++ python/0x06-python-classes/103-magic_class.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+pi = __import__('math').pi
+
+class MagicClass:
+    def __init__(self, radius):
+        if not isinstance(radius, (int, float)):
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def radius(self, value):
+        if not isinstance(value, (int , float)):
+            raise TypeError('radius must be a number')
+        self.__radius = value
+
+    def area(self):
+        return pi *self.__radius ** 2
+
+    def circumference(self):
+        return 2 * pi * self.__radius **2
+
+    def __str__(self):
+        return f"MagicClass: radius={}"
+
+mg = MagicClass(2)
+print(mg)
+
+
