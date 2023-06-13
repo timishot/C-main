e12366ef052cbc4444e477971b3c766d66c5ce01
timishot <timidotcom58@gmail.com>
2023-06-13 12:15:54 +0100

    update on animal.py

--- python/0x0A-python-inheritance/animal.py
+++ python/0x0A-python-inheritance/animal.py
@@ -39,7 +39,7 @@ class Animal:
 
 class Mammal(Animal):
     def __init__(self, birthType="born alive", appearance= "hair or fur", blooded = "Warm Blooded", nurseYoung = True):
-        Animal.__init__(self, birthtype = "Unknown", appearance = "Unknown", blooded="Unknown")
+        Animal.__init__(self, birthType , appearance , blooded)
 
 
         self.__nurseYoung = nurseYoung
@@ -53,24 +53,45 @@ class Mammal(Animal):
         self.__nurseYoung = nurseYoung
 
     def __str__(self):
-        return super().__str__() + "and it is {} they nurse their young".format(self.nurseYoung)
+        return super().__str__() + " and it is {} they nurse their young".format(self.nurseYoung)
 
 
 class Reptile(Animal):
-    def __init__(self, birthType="born in an egg or born alive", apperance= "dry scales", blooded = "Cold blooded"):
-        Animal.__init__(self, birthday, apppearance, blooded)
+    def __init__(self, birthType="born in an egg or born alive", appearance= "dry scales", blooded = "Cold blooded"):
+        Animal.__init__(self, birthType, appearance, blooded)
 
     def sumAll(self, *args):
-
+        sum = 0
         for i in args:
             sum += i
 
         return sum
 
+def getBirthType(theObject):
+    print("the {} is {}".format(type(theObject).__name__, theObject.birthType))
 
 def main():
     animal1 = Animal("born alive")
     
     print(animal1.birthType)
     print(animal1)
+    print()
+
+    mammal1 = Mammal()
+    print(mammal1.birthType)
+    print(mammal1.appearance)
+    print(mammal1.blooded)
+    print(mammal1.nurseYoung)
+    print(mammal1)
+
+
+
+    reptile1 = Reptile()
+    print(reptile1.birthType)
+
+    print(reptile1)
+    print("sum: {}".format(reptile1.sumAll(1,2,3,4,5)))
+    print()
+    getBirthType(mammal1)
+    getBirthType(reptile1)
 main()
