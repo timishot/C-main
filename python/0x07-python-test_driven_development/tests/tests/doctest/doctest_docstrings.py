b64bd41d0694196a5252f66f53c344d14b8029f8
timishot <timidotcom58@gmail.com>
2023-06-19 23:22:05 +0100

    more on doctest

---
+++ python/0x07-python-test_driven_development/tests/tests/doctest/doctest_docstrings.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+"""Tests can appear in any docstring within the module.
+Module-level tests cross class and function boundaries.
+
+>>> A('a') == B('b')
+False
+"""
+
+class A:
+    """simple class.
+
+    >>> A('instance_name').name
+    'instance_name'
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def method(self):
+        """Return an unusual value.
+
+        >>> A('name').method()
+        'eman'
+        """
+
+        return ''.join(reversed(self.name))
+
+
+class B(A):
+    """Another simple class.
+
+    >>> B('different_name').name
+    'different_name'
+    """
+
