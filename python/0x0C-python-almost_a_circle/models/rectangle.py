cf2531c191f3cf0557993d1352ded7e862396db3
timishot <timidotcom58@gmail.com>
2023-07-11 11:07:22 +0100

    created a rectangle class

---
+++ python/0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a rectangle class"""
+from models.base import Base
+class Rectangle(Base):
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width=width
+        self.__height=height
+        self.__x=x
+        self.__y=y
+        super().__init__(id)
+
+
+
+
+
