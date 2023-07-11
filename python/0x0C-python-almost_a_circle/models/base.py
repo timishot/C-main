0d36bab092646a57f041713f8381ff7c6b6b292e
timishot <timidotcom58@gmail.com>
2023-07-11 23:03:17 +0100

    That all for today

--- python/0x0C-python-almost_a_circle/models/base.py
+++ python/0x0C-python-almost_a_circle/models/base.py
@@ -35,6 +35,17 @@ class Base:
             return []
         return json.loads(json_string)
 
+    @classmethod
+    def create(cls, **dictionary):
+        if dictionary or dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new=cls(1)
+            new.update(**dictionary)
+            return new
+
+
 
 
 
