43665fb8da11277904daeb792411f43be0f7174b
timishot <timidotcom58@gmail.com>
2023-08-21 23:35:05 +0100

    more on function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,12 @@
+#include "function_pointers.h"
 
-void print_name(char *name, void (*f)(char *)){
+/**
+ * print_name -  a function that prints a name
+ * @name: the name
+ * @f: function;
+**/
+
+void print_name(char *name, void (*f)(char *))
+{
 	f(name);
-} 
+}
