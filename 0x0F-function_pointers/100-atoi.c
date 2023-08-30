3dda2a26fe84a9a28f8657ecb27f09becbbc9533
timishot <timidotcom58@gmail.com>
2023-08-30 20:26:27 +0100

    more on this

--- 0x0F-function_pointers/100-atoi.c
+++ 0x0F-function_pointers/100-atoi.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <limits.h>
 
 int _atoi(char *s)
 {
-	
+	int result = 0;
+	int sign = 1;
+
+	while (*s == ' ') /**skiping whitespace**/
+		s++;
+
+	/**dealing with minus sign**/
+	while (*s == '+' || *s == '-')
+	{
+		if (*s == '-')
+			sign *= -1;
+		s++;
+	}
+
+	/**convert string to integer**/
+	while (*s >= 0 && *s <= 9){
+		/**handle overflow**/
+		if (result > INT_MAX/10){
+			if (sign == 1)
+				return INT_MAX;
+			else 
+				return INT_MAX;
+		}
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+
+	return (result * sign);
 }
+
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+
+int _atoi(char *s);
+
+#endif
