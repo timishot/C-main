67b09d5c596e7f122b3b4e96aedf5216e91657ba
timishot <timidotcom58@gmail.com>
2023-11-07 17:36:23 +0100

    working a push function for a stack

--- stack/stackArray.c
+++ stack/stackArray.c
@@ -1,68 +1,68 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// #define N 5
-// int stack[N];
-// int top = -1;
+#define N 5
+int stack[N];
+int top = -1;
 
-// void push(){
-// 	int x;
-// 	printf("Enter data: ");
-// 	scanf("%d", &x);
-// 	if (top == N-1){
-// 		printf("Overcondition");
-// 	}else{
-// 		top++;
-// 		stack[top] = x;
+void push(){
+	int x;
+	printf("Enter data: \n");
+	scanf("%d\n", &x);
+	if (top == N-1){
+		printf("Overcondition\n");
+	}else{
+		top++;
+		stack[top] = x;
 
-// 	}
-// }
+	}
+}
 
-// void pop(){
-// 	  int item;
-// 	  if(top == -1){
-// 		printf("underflow"); 
-// 	  }else{
-// 		item = stack[top];
-// 		top--;
-// 		printf("%d", item);
-// 	  }
-// }
+void pop(){
+	  int item;
+	  if(top == -1){
+		printf("underflow\n"); 
+	  }else{
+		item = stack[top];
+		top--;
+		printf("%d\n", item);
+	  }
+}
 
-// void peek(){
-// 	if (top == -1){
-// 		printf("Underflow");
-// 	}else{
-// 		printf("%d", stack[top]);
-// 	}
-// }
+void peek(){
+	if (top == -1){
+		printf("Underflow\n");
+	}else{
+		printf("%d\n", stack[top]);
+	}
+}
 
-// void display(){
-// 	int i;
-// 	for (i =top; i >=0; i--){
-// 		printf("%d\n", stack[i]); 
-// 	}
-// }
+void display(){
+	int i;
+	for (i =top; i >=0; i--){
+		printf("%d\n", stack[i]); 
+	}
+}
 
-// void main(){
-// 	int ch;
+void main(){
+	int ch;
 	
-// 	  do{
-// 		printf("Enter choice: 1: push 2: pop 3:peek 4:display : ");
-// 		scanf("%d", &ch);
-// 		switch (ch)
-// 		{
-// 			case 1: push();
-// 				break;
-// 			case 2: pop();
-// 				break;
-// 			case 3: peek();
-// 				break;
-// 			case 4: display();
-// 				break;
-// 			default: printf("Invalid choice");
-// 		}
-// 	  }while(ch!=0);
-// 	  getchar();
+	  do{
+		printf("Enter choice: 1: push 2: pop 3:peek 4:display : ");
+		scanf("%d", &ch);
+		switch (ch)
+		{
+			case 1: push();
+				break;
+			case 2: pop();
+				break;
+			case 3: peek();
+				break;
+			case 4: display();
+				break;
+			default: printf("Invalid choice");
+		}
+	  }while(ch!=0);
+	  getchar();
 	 
-// }
+}
 
